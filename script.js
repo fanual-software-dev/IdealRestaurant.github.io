02dbe7a31ace55c008c43e8db22b14e4d6b2cc89
fanual-software-dev <asfawfanual2003@gmail.com>
2024-08-22 22:34:50 +0300

    Javascript Form added

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// const document = document.getElementsByClassName('giant-div');
-console.log('Hello World');
+// document.getElementById('form').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     submitFormAndFeedback();
+//   });
+
+function Submit_Form(){
+    let name = document.forms['form']['name'].value;
+    let email = document.forms['form']['email'].value;
+    let feedback = document.forms['form']['textarea'].value;
+
+    console.log(name,email,feedback);
+
+    const subject = 'Feedback from user';
+    const body = `Name: ${name}\n Email:${email}\n Subject: ${subject}\n Feedback:\n ${feedback}`;
+
+    const mailto = `mailto:asfawfanual2003@gmail.com?subject = ${subject} &body=${encodeURIComponent(body)}`;
+    window.location.href = mailto;
+
+}
+
