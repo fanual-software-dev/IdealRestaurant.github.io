b681b89f9c60f02a3cc190a73c8dc64c234ba059
fanual-software-dev <asfawfanual2003@gmail.com>
2024-08-23 00:26:47 +0300

    Form submit enabled

--- script.js
+++ script.js
@@ -3,18 +3,14 @@
 //     submitFormAndFeedback();
 //   });
 
-function Submit_Form(){
-    let name = document.forms['form']['name'].value;
-    let email = document.forms['form']['email'].value;
-    let feedback = document.forms['form']['textarea'].value;
+function Submit_Form
+    (message) {
+        if (message==='OK'){
+            swal("Email Sent Successfully !", "You clicked the button!", "success");
+        }
 
-    console.log(name,email,feedback);
-
-    const subject = 'Feedback from user';
-    const body = `Name: ${name}\n Email:${email}\n Subject: ${subject}\n Feedback:\n ${feedback}`;
-
-    const mailto = `mailto:asfawfanual2003@gmail.com?subject = ${subject} &body=${encodeURIComponent(body)}`;
-    window.location.href = mailto;
-
+        else{
+            swal("Opps email not sent. Please try again !", "You clicked the button!", "error");  
+        }
 }
 
